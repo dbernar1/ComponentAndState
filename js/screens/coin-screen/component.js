8b8d10f6134dd3fb7bde914ef0c44787fa78ff56
Dan Bernardic <dan.bernardic@gmail.com>
2018-03-28 15:50:25 -0500

    Added Navigation to coin detail page

--- js/screens/coin-screen/component.js
+++ js/screens/coin-screen/component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text, FlatList, Image, } from 'react-native';
+import { TouchableOpacity, ScrollView, View, Text, FlatList, Image, } from 'react-native';
 import styles from './style.js';
 import LoadingScreen from './components/loading'
 import { connecty } from '../../redux';
@@ -8,17 +8,16 @@ const Coins = connecty(
 	[ 'coinData', 'refreshing', ],
 	[ 'refreshCoinData', 'loadMore', ]
 )( ( {
-	coinData, refreshCoinData, refreshing, loadMore,
+	coinData, refreshCoinData, refreshing, loadMore, navigation,
 } ) => <View style={styles.container}>
 	<FlatList
-		ListHeaderComponent={ <AppHeading text="Coin Prices" /> }
 		ListFooterComponent={ <LoadingScreen /> }
 		refreshing={ refreshing }
 		onRefresh={ refreshCoinData }
 		onEndReached={ loadMore }
 		onEndReachedThreshold={ 0.7 }
 		data={ coinData }
-		renderItem={ ( { item } ) => <CoinPriceItem { ...item } /> }
+		renderItem={ ( { item } ) => <CoinPriceItem navigation={ navigation } { ...item } /> }
 		keyExtractor={ item => item.id }
 	/>
 </View> );
@@ -34,10 +33,7 @@ export default Coins;
 
 
 // Details...
-
-const AppHeading = ( { text } ) => <Text style={ styles.appHeading }>{ text.toUpperCase() }</Text>;
-
-const CoinImage = ( { name } ) => <Image
+export const CoinImage = ( { name } ) => <Image
 	source={ { uri: `https://coincodex.com/en/resources/images/admin/coins/${ name.toLowerCase().replace( ' ', '-' ) }.png:resizebox?56x56`, } }
 	style={ styles.coinImage }
 />;
@@ -47,17 +43,24 @@ const CoinName = ( { children } ) => <Text style={ styles.coinName }>{ children 
 const CoinPrice = ( { children } ) => <Text style={ styles.coinPrice }>{ children } $</Text>;
 const CoinPercentChange24H = ( { children } ) => <View style={ styles.flexRow } ><Text style={ styles.boldText }>24h: </Text><Text style={ styles.coinPercentChange24H }>{ children } %</Text></View>;
 
-const CoinPriceItem = ( { symbol, name, price_usd, percent_change_24h, } ) => <View style={ styles.coinPriceItem }>
-	<View style={ { flex: 1, } }>
-		<CoinImage name={ name } />
-	</View>
-	<View style={ [ styles.flexRow, { flex: 4, }, ] }>
-		<CoinSymbol>{ symbol }</CoinSymbol>
-		<Text> | </Text>
-		<CoinName>{ name }</CoinName>
-	</View>
-	<View style={ [ styles.flexRow, { flex: 6, justifyContent: 'flex-end', }, ] }>
-		<CoinPrice>{ price_usd }</CoinPrice>
-		<CoinPercentChange24H>{ percent_change_24h }</CoinPercentChange24H>
-	</View>
+const CoinPriceItem = ( { navigation, symbol, name, price_usd, percent_change_24h, } ) => <View style={ styles.coinPriceItem }>
+	<TouchableOpacity onPress={ () => {
+		navigation.navigate( 'CoinDetail', {
+			symbol,
+			name,
+		} );
+	} }>
+		<View style={ { flex: 1, } }>
+			<CoinImage name={ name } />
+		</View>
+		<View style={ [ styles.flexRow, { flex: 4, }, ] }>
+			<CoinSymbol>{ symbol }</CoinSymbol>
+			<Text> | </Text>
+			<CoinName>{ name }</CoinName>
+		</View>
+		<View style={ [ styles.flexRow, { flex: 6, justifyContent: 'flex-end', }, ] }>
+			<CoinPrice>{ price_usd }</CoinPrice>
+			<CoinPercentChange24H>{ percent_change_24h }</CoinPercentChange24H>
+		</View>
+	</TouchableOpacity>
 </View>;
--- js/screens/coin-screen/index.js
+++ js/screens/coin-screen/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import LoadingScreen from './components/loading'
 import Coins from './component.js'
-import { Provider, } from 'react-redux';
+import { Provider, connect, } from 'react-redux';
 import { createMyStore, connecty, } from '../../redux';
+import { StackNavigator } from 'react-navigation';
+import { findWhere } from 'underscore';
+import { CoinImage } from './component';
 
 const myStore = createMyStore();
 
 const App = () => <Provider store={ myStore }>
-	<CoinScreenContainer />
+	<Nav />
 </Provider>;
 
 export default App;
@@ -30,6 +33,10 @@ const CoinScreenContainer = connecty(
 	[ 'initialLoading', ],
 	[ 'loadInitialData', ],
 )( class extends Component {
+	static navigationOptions = {
+		title: 'Coin Prices',
+	};
+
 	componentDidMount() {
 		this.props.loadInitialData();
 	}
@@ -38,7 +45,39 @@ const CoinScreenContainer = connecty(
 		if ( this.props.initialLoading ) {
 			return <LoadingScreen />;
 		} else {
-			return <Coins />;
+			return <Coins navigation={ this.props.navigation } />;
 		}
 	}
+} );
+
+const CoinDetail = connect(
+	(
+		{ coinData },
+		{ navigation }
+	) => {
+		
+		return {
+			coin: findWhere(
+				coinData,
+				{ symbol: navigation.state.params.symbol, }
+			),
+		};
+	}
+)( class extends Component {
+	static navigationOptions = ( { navigation } ) => ( {
+		title: navigation.state.params.name,
+	} );
+
+	render() {
+		return <CoinImage name={ this.props.coin.name } />;
+	}
+} );
+
+const Nav = StackNavigator( {
+	CoinList: {
+		screen: CoinScreenContainer,
+	},
+	CoinDetail: {
+		screen: CoinDetail,
+	},
 } );
